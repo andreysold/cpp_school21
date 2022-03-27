e65b41a132db18a95181d91443559d4bea2ea3da
Andrey <forstudyandrey4@gmail.com>
2022-03-28 00:51:37 +0300

    wrgfs

--- day01/ex04/ReplaceString.hpp
+++ day01/ex04/ReplaceString.hpp
@@ -6,8 +6,6 @@ class ReplaceString
         std::string file_name;
         std::string s1;
         std::string s2;
-        std::string word1;
-        std::string word2;
     public:
         ReplaceString(std::string file_name);
         void    ReplaceStr(std::string s1, std::string s2);
